e6d566528b53a7169146b3ac1ae8f373780bbaf5
schumika <schumika@gmail.com>
2013-09-19 20:06:22 +0300

    readed files

---
+++ Scores/Classes/Controllers/AJGamesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  AJGamesTableViewController.h
+//  Scores
+//
+//  Created by Anca Calugar on 9/18/13.
+//  Copyright (c) 2013 AJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJGamesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *gamesArray;
+
+@end
