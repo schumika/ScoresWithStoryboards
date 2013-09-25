23f9f9ca127a77c413317ffd760d6c30a92d4db5
Anca <schumika@gmail.com>
2013-09-25 15:12:43 +0300

    added support for custom fonts

---
+++ Scores/UIFont+Additions.h
@@ -0,0 +1,16 @@
+//
+//  UIFont+Additions.h
+//  FontTrial
+//
+//  Created by Anca Calugar on 1/24/13.
+//  Copyright (c) 2013 Anca Julean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (Additions)
+
++ (UIFont *)LDBrushFontWithSize:(CGFloat)size;
++ (UIFont *)faranvaleFontWithSize:(CGFloat)size;
+
+@end
